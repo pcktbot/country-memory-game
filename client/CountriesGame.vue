a72ec7ba1b6d84275d9a82aa3395dd03fecf2a6f
pcktbot <pcktbot@gmail.com>
2026-06-05 15:23:16 -0500

    feat: extract CountriesGame and UsGame as standalone route components

---
+++ client/CountriesGame.vue
@@ -0,0 +1,13 @@
+<template>
+  <MapGame
+    :items="countries"
+    map-url="world.svg"
+    completed-label="All countries completed!"
+    :show-flag="true"
+  />
+</template>
+
+<script setup lang="ts">
+import MapGame from './MapGame.vue'
+import { countries } from './countries'
+</script>
---
+++ client/UsGame.vue
@@ -0,0 +1,13 @@
+<template>
+  <MapGame
+    :items="states"
+    map-url="us.svg"
+    completed-label="All states completed!"
+    :show-flag="false"
+  />
+</template>
+
+<script setup lang="ts">
+import MapGame from './MapGame.vue'
+import { states } from './states'
+</script>
